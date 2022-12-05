b31deddaf9c36bb44c7d1ff881c15c8cc0bfca5b
Faith Johnson <fmjohnson97@aol.com>
2022-12-05 16:36:21 -0500

    computing angles for path

--- navigationConfig.py
+++ navigationConfig.py
@@ -1,5 +1,8 @@
 from navigation.simple_controller import SimpleController
+import numpy as np
 
 robot_controller = SimpleController()
 
 obstacle_map_threshold = 25  # ToDO: play around with this obstacle threshold
+
+trash_pos_offset = np.array([0, 0.5])
--- navigationModule.py
+++ navigationModule.py
@@ -387,31 +387,6 @@ def getObstacleMap():
     return data, pose
 
 
-# def OLDplanPath(start, end, obstacle_coords, map_size):
-#     # based on this https://pypi.org/project/pyhpp/
-#     scenario = {
-#         # z=0 to make it a 2d mapping problem
-#         "dimension": {"x": map_size[0], "y": map_size[1], "z": 0},
-#         "waypoint": {
-#             "start": {"x": start[0], "y": start[1], "z": 0},
-#             "stop": {"x": end[0], "y" : end[1], "z": 0},
-#             "allowDiagonal": False
-#             },
-#         "data": {
-#             "size": len(obstacle_coords[0]),
-#             "x": obstacle_coords[0],
-#             "y": obstacle_coords[1],
-#             },
-#         "boundary": {
-#             "zCeil": 6,
-#             "zFloor": 1
-#             }
-#     }
-#     a_star = AStar(scenario)
-#     result = a_star.calculate_path()
-#     path = np.stack([result['path']['x'], result['path']['y']]).T
-#     return path #returns the int coords of where to go to avoid obstacles and reach goal
-
 def planPath(start, end, occupancy_map):
     # load the map
     #the whole thing is 4 meters, so can divde by the number of grids?; should give value in meters
@@ -437,50 +412,32 @@ def planPath(start, end, occupancy_map):
 
 
 def followPath(path):
-    #this path is the global position, not relative to the previous position
-    # also it's in discrete grid mode (so like image coords), and not in meters
-    #TODO:preprocess path so it's in the format we need
-    #TODO: also add an angle to it
-    for p in path:
+    dist_path = np.diff(path)
+    path_angles = [np.arctan2(point[-1] / point[-2]) for point in dist_path]
+    new_path = np.hstack([dist_path, path_angles])
+    for p in new_path:
         robot_controller.step(p)
-    pass
+
 
 def navToPointsFound(people, trash_point, maybes):
-    trash_pos_offset = 0 #TODO: do we need to be offset from the trash at all?, also move this to config
-    # goals = [np.mean(t,axis=0) for t in trash] #not exactly sure what shape trash is going to have for this to work
-
-    # for g in goals:
     obstacle_map, pose = getObstacleMap()
-    # process obstacle map to get x,y points of the obstacles for the a* algorithm
-    # obstacle_coords = [[], []]
-    # for i in range(obstacle_map.shape[0]):
-    #     for j in range(obstacle_map.shape[1]):
-    #         if obstacle_map[i,j]> obstacle_map_threshold:
-    #             obstacle_coords[0].append(i)
-    #             obstacle_coords[1].append(j)
 
-    start = (2,0) #ToDO: get robot start point
-    path = planPath(start, trash_point[:-1] , obstacle_map)
-    # if path:
-    #     followPath(path)
+    start = (2,0) #ToDO: get robot start point # (horizonal #,  vertical #)
+    path = planPath(start, trash_point-trash_pos_offset , obstacle_map)
+    if path:
+        followPath(path)
 
 
 def continueNavCircuit():
     obstacle_map = getObstacleMap()
     # process obstacle map to get x,y points of the obstacles for the a* algorithm
-    obstacle_coords = [[], []]
-    for i in range(obstacle_map.shape[0]):
-        for j in range(obstacle_map.shape[1]):
-            if obstacle_map[i, j] > obstacle_map_threshold:
-                obstacle_coords[0].append(i)
-                obstacle_coords[1].append(j)
 
     #TODO: pick a random empty point from the obstacle map
     end = 0 #random empty point here
 
     #TODO: plan path to there
     start = 0 #ToDO: get robot start point
-    path = planPath(start, end, obstacle_coords, obstacle_map.shape)
+    path = planPath(start, end, obstacle_map
     followPath(path)
 
 # if __name__=='__main__':
